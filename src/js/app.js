2256b884e2747da1a44eea78d2555eeb67ac6a6f
Regina Salikhova <yanadera1873@gmail.com>
2025-06-22 19:31:52 +0400

    fix variables

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 // Отвечает за запуск приложения
 
-import Game, {GameSavingData, readGameSaving as saveGame, writeGameSaving as loadGame} from "./game.js";
+import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from "./game.js";
 
 const game = new Game();
 game.start();
